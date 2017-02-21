c35c0c32a9dc741566755602cfe308dd2d43e7cc
silviapineda <silviapinedasanjuan@gmail.com>
2017-02-21 15:57:35 -0800

    Change the files to call the ANNOVAR

--- Annovar.sh
+++ Annovar.sh
@@ -16,7 +16,7 @@ perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar snp138 humand
 perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar ljb26_all humandb/
 
 #Create the input files from the vcf files
-perl convert2annovar.pl -format vcf4 -allsample -withfreq ../../ExomeSeq/VCF/gVCF/joint.output.snp.indel.recal.filtered.biallelic.SNPs.vcf  > ../../ExomeSeq/ANNOVAR/joint.output.snp.indel.recal.filtered.biallelic.SNPs.avinput
+perl convert2annovar.pl -format vcf4 -allsample -withfreq /home/pinedasans/ExomeSeq/VCF/joint.output.snp.indel.recal.filtered.biallelic.vcf  > /home/pinedasans/ExomeSeq/VCF/joint.output.snp.indel.recal.filtered.biallelic.avinput
 
 #Create the annotation
-perl table_annovar.pl /home/pinedasans/ExomeSeq/ANNOVAR/joint.output.snp.indel.recal.filtered.biallelic.SNPs.avinput humandb/ -buildver hg19 -out myanno.joint.output.snp.indel.recal.QC.avinput -remove -protocol refGene,cytoBand,genomicSuperDups,esp6500siv2_all,1000g2014oct_all,1000g2014oct_afr,1000g2014oct_eas,1000g2014oct_eur,snp138,ljb26_all -operation g,r,r,f,f,f,f,f,f,f -nastring . -csvout
+perl table_annovar.pl /home/pinedasans/ExomeSeq/VCF/joint.output.snp.indel.recal.filtered.biallelic.avinput humandb/ -buildver hg19 -out myanno.joint.output.snp.indel.recal.avinput -remove -protocol refGene,cytoBand,genomicSuperDups,esp6500siv2_all,1000g2014oct_all,1000g2014oct_afr,1000g2014oct_eas,1000g2014oct_eur,snp138,ljb26_all -operation g,r,r,f,f,f,f,f,f,f -nastring . -csvout
