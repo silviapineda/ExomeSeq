a42f48f756601f8579b056c7823b9035e4c6b44a
silviapineda <silviapinedasanjuan@gmail.com>
2017-02-22 12:11:09 -0800

    To obtain the variants we have in the 1000G vcf files

--- grepVariants_1000GVCF.py
+++ grepVariants_1000GVCF.py
@@ -1,11 +1,16 @@
-with open('/~/ExomeSeq/1000G/variantList_vcf.txt') as f:
+with open('/home/pinedasans/ExomeSeq/1000G/variantList_vcf.txt') as f:
     lines1 = f.readlines()
-    #print lines1
+    #print (lines1)
   
-fout = open('/~/ExomeSeq/1000G/1000G_genotypes_variantList.vcf','w')
+fout = open('/home/pinedasans/ExomeSeq/1000G/1000G_genotypes_variantList.vcf','w')
 
-with open('/~/ExomeSeq/1000G/1000G_genotypes.vcf') as f:
-  for line in f:
-    if any(line.startswith(x.strip()) for x in lines1):
-        print line
-        fout.write( line + '\n')
+for i in range(1,22):
+    #print (i)
+    with open("/home/pinedasans/ExomeSeq/1000G/ALL.chr" + str(i) + ".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf") as f:
+        print (f)
+        for line in f:
+            if any(line.startswith(x.strip()) for x in lines1):
+                #print (line)
+                fout.write( line + '\n')
+        
+        
